3bf154effed14e40d07da1796ad40b6831379c9e
Jacopo Filié <Jackfelle@users.noreply.github.com>
2016-11-24 01:57:21 +0100

    MOD: completed the activity indicator view implementation.

--- Project/Sources/User Interface/JFActivityIndicatorView.m
+++ Project/Sources/User Interface/JFActivityIndicatorView.m
@@ -47,16 +47,16 @@ NS_ASSUME_NONNULL_BEGIN
 @property (strong, nonatomic, readonly)	NSMutableArray<NSLayoutConstraint*>*	constraints;
 
 // MARK: Properties - User interface (Flags)
-@property (assign, nonatomic, getter=isButtonHidden)		BOOL	buttonHidden;
-@property (assign, nonatomic, getter=isIndicatorViewHidden)	BOOL	indicatorViewHidden;
-@property (assign, nonatomic, getter=isTextLabelHidden)		BOOL	textLabelHidden;
+@property (assign, nonatomic, getter=isButtonHidden)			BOOL	buttonHidden;
+@property (assign, nonatomic, getter=isContainerImageHidden)	BOOL	containerImageHidden;
+@property (assign, nonatomic, getter=isIndicatorViewHidden)		BOOL	indicatorViewHidden;
+@property (assign, nonatomic, getter=isTextLabelHidden)			BOOL	textLabelHidden;
 
 // MARK: Methods - Memory management
 + (void)	initializeProperties:(JFActivityIndicatorView*)object;
 
 // MARK: Methods - User interface management
 - (void)	toggleAnimation;
-- (void)	updateDisplay;
 
 @end
 NS_ASSUME_NONNULL_END
@@ -119,10 +119,11 @@ NS_ASSUME_NONNULL_BEGIN
 // MARK: Properties - User interface (Flags)
 // =================================================================================================
 
-@synthesize buttonHidden		= _buttonHidden;
-@synthesize containerHidden		= _containerHidden;
-@synthesize indicatorViewHidden	= _indicatorViewHidden;
-@synthesize textLabelHidden		= _textLabelHidden;
+@synthesize buttonHidden			= _buttonHidden;
+@synthesize containerImageHidden	= _containerImageHidden;
+@synthesize containerHidden			= _containerHidden;
+@synthesize indicatorViewHidden		= _indicatorViewHidden;
+@synthesize textLabelHidden			= _textLabelHidden;
 
 // =================================================================================================
 // MARK: Properties accessors - Data
@@ -185,38 +186,191 @@ NS_ASSUME_NONNULL_BEGIN
 }
 
 // =================================================================================================
-// MARK: Properties - User interface (Appearance)
+// MARK: Properties accessors - User interface (Appearance)
 // =================================================================================================
 
 - (void)setBackgroundColor:(UIColor* __nullable)backgroundColor
-{}
+{
+	if(!backgroundColor)
+		backgroundColor = JFColorAlpha(80);
+	
+	if(JFAreObjectsEqual(super.backgroundColor, backgroundColor))
+		return;
+	
+	super.backgroundColor = backgroundColor;
+}
 
 - (void)setButtonTitleColor:(UIColor* __nullable)buttonTitleColor
-{}
+{
+	if(JFAreObjectsEqual(_buttonTitleColor, buttonTitleColor))
+		return;
+	
+	_buttonTitleColor = buttonTitleColor;
+	
+	if(self.window)
+		[self.button setTitleColor:buttonTitleColor forState:UIControlStateNormal];
+}
 
 - (void)setContainerBackgroundColor:(UIColor* __nullable)containerBackgroundColor
-{}
+{
+	if(!containerBackgroundColor)
+		containerBackgroundColor = JFColorAlpha(192);
+	
+	if(JFAreObjectsEqual(_containerBackgroundColor, containerBackgroundColor))
+		return;
+	
+	_containerBackgroundColor = containerBackgroundColor;
+	
+	if(self.window)
+		self.containerView.backgroundColor = containerBackgroundColor;
+}
 
 - (void)setContainerBackgroundImage:(UIImage* __nullable)containerBackgroundImage
-{}
+{
+	if(JFAreObjectsEqual(_containerBackgroundImage, containerBackgroundImage))
+		return;
+	
+	_containerBackgroundImage = containerBackgroundImage;
+	
+	self.containerImageHidden = !containerBackgroundImage;
+	
+	if(self.window)
+		self.containerImageView.image = containerBackgroundImage;
+}
 
 - (void)setContainerCornerRadius:(CGFloat)containerCornerRadius
-{}
+{
+	if(_containerCornerRadius == containerCornerRadius)
+		return;
+	
+	_containerCornerRadius = containerCornerRadius;
+	
+	if(self.window)
+		self.containerView.layer.cornerRadius = containerCornerRadius;
+}
 
 - (void)setIndicatorColor:(UIColor* __nullable)indicatorColor
-{}
+{
+	if(!indicatorColor)
+		indicatorColor = [UIColor whiteColor];
+	
+	if(JFAreObjectsEqual(_indicatorColor, indicatorColor))
+		return;
+	
+	_indicatorColor = indicatorColor;
+	
+	if(self.window)
+		self.indicatorView.color = indicatorColor;
+}
 
 - (void)setIndicatorImagesAnimationDuration:(NSTimeInterval)indicatorImagesAnimationDuration
-{}
+{
+	if(_indicatorImagesAnimationDuration == indicatorImagesAnimationDuration)
+		return;
+	
+	_indicatorImagesAnimationDuration = indicatorImagesAnimationDuration;
+	
+	if(self.window)
+		self.imageView.animationDuration = indicatorImagesAnimationDuration;
+}
 
 - (void)setIndicatorImagesSize:(CGSize)indicatorImagesSize
-{}
+{
+	if(CGSizeEqualToSize(_indicatorImagesSize, indicatorImagesSize))
+		return;
+	
+	_indicatorImagesSize = indicatorImagesSize;
+	
+	if(self.window)
+		[self setNeedsUpdateConstraints];
+}
 
 - (void)setIndicatorStyle:(UIActivityIndicatorViewStyle)indicatorStyle
-{}
+{
+	if(_indicatorStyle == indicatorStyle)
+		return;
+	
+	_indicatorStyle = indicatorStyle;
+	
+	if(self.window)
+		[self setNeedsLayout];
+}
 
 - (void)setTextColor:(UIColor* __nullable)textColor
-{}
+{
+	if(!textColor)
+		textColor = [UIColor whiteColor];
+	
+	_textColor = textColor;
+	
+	if(self.window)
+		self.textLabel.textColor = textColor;
+}
+
+// =================================================================================================
+// MARK: Properties accessors - User interface (Flags)
+// =================================================================================================
+
+- (void)setButtonHidden:(BOOL)buttonHidden
+{
+	if(_buttonHidden == buttonHidden)
+		return;
+	
+	_buttonHidden = buttonHidden;
+	
+	if(self.window)
+		[self setNeedsLayout];
+}
+
+- (void)setContainerHidden:(BOOL)containerHidden
+{
+	if(_containerHidden == containerHidden)
+		return;
+	
+	_containerHidden = containerHidden;
+	
+	if(self.window)
+		[self setNeedsLayout];
+}
+
+- (void)setContainerImageHidden:(BOOL)containerImageHidden
+{
+	if(_containerImageHidden == containerImageHidden)
+		return;
+	
+	_containerImageHidden = containerImageHidden;
+	
+	if(self.window)
+		[self setNeedsLayout];
+}
+
+- (void)setHidden:(BOOL)hidden
+{
+	super.hidden = hidden;
+	[self toggleAnimation];
+}
+
+- (void)setIndicatorViewHidden:(BOOL)indicatorViewHidden
+{
+	if(_indicatorViewHidden == indicatorViewHidden)
+		return;
+	
+	_indicatorViewHidden = indicatorViewHidden;
+	
+	if(self.window)
+		[self setNeedsLayout];
+}
+
+- (void)setTextLabelHidden:(BOOL)textLabelHidden
+{
+	if(_textLabelHidden == textLabelHidden)
+		return;
+	
+	_textLabelHidden = textLabelHidden;
+	
+	if(self.window)
+		[self setNeedsLayout];
+}
 
 // =================================================================================================
 // MARK: Methods - Memory management
@@ -225,14 +379,10 @@ NS_ASSUME_NONNULL_BEGIN
 + (void)initializeProperties:(JFActivityIndicatorView*)object
 {
 	// User interface (Appearance)
-	object->_containerBackgroundColor			= [UIColor blackColor];
-	object->_containerBackgroundImage			= nil;
 	object->_containerCornerRadius				= 8;
-	object->_indicatorColor						= [UIColor whiteColor];
 	object->_indicatorImagesAnimationDuration	= 0;
 	object->_indicatorImagesSize				= CGSizeZero;
 	object->_indicatorStyle						= UIActivityIndicatorViewStyleWhiteLarge;
-	object->_textColor							= [UIColor lightTextColor];
 	
 	// User interface (Constraints)
 	object->_constraints	= [NSMutableArray<NSLayoutConstraint*> new];
@@ -244,9 +394,14 @@ NS_ASSUME_NONNULL_BEGIN
 	object->_textLabelHidden		= YES;
 	
 	// User interface (Inherited)
-	object.backgroundColor = nil;
 	object.opaque = NO;
 	object.userInteractionEnabled = YES;
+	
+	// Resets the resettable properties.
+	object.backgroundColor			= nil;
+	object.containerBackgroundColor	= nil;
+	object.indicatorColor			= nil;
+	object.textColor				= nil;
 }
 
 - (instancetype __nullable)initWithCoder:(NSCoder*)aDecoder
@@ -274,11 +429,272 @@ NS_ASSUME_NONNULL_BEGIN
 // MARK: Methods - User interface management
 // =================================================================================================
 
-- (void)toggleAnimation
-{}
+- (void)layoutSubviews
+{
+	[super layoutSubviews];
+	
+	UIButton* button = self.button;
+	UIView* containerView = self.containerView;
+	UIImageView* containerImageView = self.containerImageView;
+	UIImageView* imageView = self.imageView;
+	UIActivityIndicatorView* indicatorView = self.indicatorView;
+	UILabel* textLabel = self.textLabel;
+	
+	if([self isContainerHidden])
+	{
+		// Removes everything.
+		[button removeFromSuperview];
+		[containerView removeFromSuperview];
+		[containerImageView removeFromSuperview];
+		[imageView removeFromSuperview];
+		[indicatorView removeFromSuperview];
+		[textLabel removeFromSuperview];
+	}
+	else
+	{
+		// Layouts the container view.
+		if(!containerView)
+		{
+			containerView = [UIView new];
+			containerView.backgroundColor = self.containerBackgroundColor;
+			containerView.layer.cornerRadius = self.containerCornerRadius;
+			containerView.opaque = NO;
+			containerView.translatesAutoresizingMaskIntoConstraints = NO;
+			
+			self.containerView = containerView;
+			[self addSubview:containerView];
+		}
+		
+		// Layouts the container image view.
+		if([self isContainerImageHidden])
+			[containerImageView removeFromSuperview];
+		else
+		{
+			if(!containerImageView)
+			{
+				containerImageView = [UIImageView new];
+				containerImageView.image = self.containerBackgroundImage;
+				containerImageView.translatesAutoresizingMaskIntoConstraints = NO;
+				[containerView addSubview:containerImageView];
+				self.containerImageView = containerImageView;
+			}
+			[containerView addSubview:containerImageView];
+		}
+		
+		// Layouts the button.
+		if([self isButtonHidden])
+			[button removeFromSuperview];
+		else
+		{
+			if(!button)
+			{
+				button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+				button.jf_actionBlock = self.buttonActionBlock;
+				button.translatesAutoresizingMaskIntoConstraints = NO;
+				[button setTitle:self.buttonTitle forState:UIControlStateNormal];
+				[button setTitleColor:self.buttonTitleColor forState:UIControlStateNormal];
+				self.button = button;
+			}
+			[containerView addSubview:button];
+		}
+		
+		if([self isIndicatorViewHidden])
+		{
+			// Layouts the image view.
+			[indicatorView removeFromSuperview];
+			if(!imageView)
+			{
+				imageView = [UIImageView new];
+				imageView.animationDuration = self.indicatorImagesAnimationDuration;
+				imageView.animationImages = self.indicatorImages;
+				imageView.translatesAutoresizingMaskIntoConstraints = NO;
+				self.imageView = imageView;
+			}
+			[containerView addSubview:imageView];
+		}
+		else
+		{
+			// Layouts the indicator view.
+			[imageView removeFromSuperview];
+			UIActivityIndicatorViewStyle style = self.indicatorStyle;
+			if(!indicatorView || (indicatorView.activityIndicatorViewStyle != style))
+			{
+				if(indicatorView)
+					[indicatorView removeFromSuperview];
+				
+				indicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:style];
+				indicatorView.color = self.indicatorColor;
+				indicatorView.translatesAutoresizingMaskIntoConstraints = NO;
+				self.indicatorView = indicatorView;
+			}
+			[containerView addSubview:indicatorView];
+		}
+		
+		// Layouts the text label.
+		if([self isTextLabelHidden])
+			[textLabel removeFromSuperview];
+		else
+		{
+			if(!textLabel)
+			{
+				textLabel = [UILabel new];
+				textLabel.backgroundColor = [UIColor clearColor];
+				textLabel.numberOfLines = 0;
+				textLabel.opaque = NO;
+				textLabel.text = self.text;
+				textLabel.textAlignment = NSTextAlignmentCenter;
+				textLabel.textColor = self.textColor;
+				textLabel.translatesAutoresizingMaskIntoConstraints = NO;
+				self.textLabel = textLabel;
+			}
+			[containerView addSubview:textLabel];
+		}
+	}
+	
+	[self toggleAnimation];
+	[self setNeedsUpdateConstraints];
+}
 
-- (void)updateDisplay
-{}
+- (void)toggleAnimation
+{
+	UIImageView* imageView = self.imageView;
+	UIActivityIndicatorView* indicatorView = self.indicatorView;
+	
+	if([self isIndicatorViewHidden])
+	{
+		[indicatorView stopAnimating];
+		
+		if([self isHidden])
+			[imageView stopAnimating];
+		else
+			[imageView startAnimating];
+	}
+	else
+	{
+		[imageView stopAnimating];
+		
+		if([self isHidden])
+			[indicatorView stopAnimating];
+		else
+			[indicatorView startAnimating];
+	}
+}
+
+// =================================================================================================
+// MARK: Methods - User interface management (Constraints)
+// =================================================================================================
+
+- (void)updateConstraints
+{
+	if(self.window)
+	{
+		NSMutableArray<NSLayoutConstraint*>* constraints = [self.constraints mutableCopy];
+		
+		// Removes the old constraints.
+		[self removeConstraints:constraints];
+		[constraints removeAllObjects];
+		
+		if(![self isContainerHidden])
+		{
+			BOOL buttonHidden = [self isButtonHidden];
+			BOOL containerImageHidden = [self isContainerImageHidden];
+			BOOL indicatorViewHidden = [self isIndicatorViewHidden];
+			BOOL textLabelHidden = [self isTextLabelHidden];
+			
+			CGSize indicatorImagesSize = self.indicatorImagesSize;
+			
+			UIView* button = self.button;
+			UIView* containerImageView = self.containerImageView;
+			UIView* containerView = self.containerView;
+			UIView* indicatorView = (indicatorViewHidden ? self.imageView : self.indicatorView);
+			UIView* textLabel = self.textLabel;
+			
+			// Prepares some constraint shortcuts.
+			static __unused NSLayoutAttribute ab = NSLayoutAttributeBottom;
+			static __unused NSLayoutAttribute al = NSLayoutAttributeLeft;
+			static __unused NSLayoutAttribute ar = NSLayoutAttributeRight;
+			static __unused NSLayoutAttribute at = NSLayoutAttributeTop;
+			static __unused NSLayoutAttribute ah = NSLayoutAttributeHeight;
+			static __unused NSLayoutAttribute aw = NSLayoutAttributeWidth;
+			static __unused NSLayoutAttribute an = NSLayoutAttributeNotAnAttribute;
+			static __unused NSLayoutAttribute ax = NSLayoutAttributeCenterX;
+			static __unused NSLayoutAttribute ay = NSLayoutAttributeCenterY;
+			static __unused NSLayoutRelation re = NSLayoutRelationEqual;
+			static __unused NSLayoutRelation rg = NSLayoutRelationGreaterThanOrEqual;
+			static __unused NSLayoutRelation rl = NSLayoutRelationLessThanOrEqual;
+			
+			// Prepares the needed info.
+			NSDictionary* metrics = @{@"containerOffset":@10, @"offset":@15, @"indicatorImagesWidth":@(indicatorImagesSize.width), @"indicatorImagesHeight":@(indicatorImagesSize.height)};
+			NSMutableDictionary* views = [NSMutableDictionary dictionaryWithCapacity:5];
+			if(button)				views[@"button"]				= button;
+			if(containerImageView)	views[@"containerImageView"]	= containerImageView;
+			if(containerView)		views[@"containerView"]			= containerView;
+			if(indicatorView)		views[@"indicatorView"]			= indicatorView;
+			if(textLabel)			views[@"textLabel"]				= textLabel;
+			
+			// Creates the container view constraints.
+			UIView* v1 = containerView;
+			UIView* v2 = containerView.superview;
+			[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(>=containerOffset@999)-[containerView(0@1)]-(>=containerOffset@999)-|" options:0 metrics:metrics views:views]]; // Left, Right & Width
+			[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=containerOffset@999)-[containerView(0@1)]-(>=containerOffset@999)-|" options:0 metrics:metrics views:views]]; // Top, Bottom & Height
+			[constraints addObject:[NSLayoutConstraint constraintWithItem:v1 attribute:ax relatedBy:re toItem:v2 attribute:ax multiplier:1.0f constant:0.0f]]; // Center X
+			[constraints addObject:[NSLayoutConstraint constraintWithItem:v1 attribute:ay relatedBy:re toItem:v2 attribute:ay multiplier:1.0f constant:0.0f]]; // Center Y
+			
+			if(!containerImageHidden)
+			{
+				// Creates the container image view constraints.
+				[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(0)-[containerImageView]-(0)-|" options:0 metrics:metrics views:views]]; // Left & Right
+				[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(0)-[containerImageView]-(0)-|" options:0 metrics:metrics views:views]]; // Top & Bottom
+			}
+			
+			// Creates the indicator/image view constraints.
+			v1 = indicatorView;
+			v2 = containerView;
+			if(indicatorViewHidden)
+			{
+				// Creates the image view constraints.
+				[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(>=offset)-[indicatorView(indicatorImagesWidth)]-(>=offset)-|" options:0 metrics:metrics views:views]]; // Left, Right & Width
+				[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(offset)-[indicatorView(indicatorImagesHeight)]" options:0 metrics:metrics views:views]]; // Top & Height
+			}
+			else
+			{
+				// Creates the indicator view constraints.
+				[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(>=offset)-[indicatorView]-(>=offset)-|" options:0 metrics:metrics views:views]]; // Left & Right
+				[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(offset)-[indicatorView]" options:0 metrics:metrics views:views]]; // Top
+			}
+			[constraints addObject:[NSLayoutConstraint constraintWithItem:v1 attribute:ax relatedBy:re toItem:v2 attribute:ax multiplier:1.0f constant:0.0f]]; // Center X
+			if(buttonHidden && textLabelHidden)
+				[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[indicatorView]-(offset)-|" options:0 metrics:metrics views:views]]; // Bottom
+			
+			if(!textLabelHidden)
+			{
+				// Creates the text label constraints.
+				[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(offset)-[textLabel]-(offset)-|" options:0 metrics:metrics views:views]]; // Left & Right
+				[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[indicatorView]-(20)-[textLabel]" options:0 metrics:metrics views:views]]; // Top
+				if(buttonHidden)
+					[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[textLabel]-(offset)-|" options:0 metrics:metrics views:views]]; // Bottom
+			}
+			
+			if(!buttonHidden)
+			{
+				// Creates the button constraints.
+				v1 = button;
+				v2 = containerView;
+				[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(>=offset)-[button]-(>=offset)-|" options:0 metrics:metrics views:views]]; // Left & Right
+				NSString* formatString = [NSString stringWithFormat:@"V:[%@]-(25)-[button]-(offset)-|", (textLabelHidden ? @"indicatorView" : @"textLabel")];
+				[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:formatString options:0 metrics:metrics views:views]]; // Top & Bottom
+				[constraints addObject:[NSLayoutConstraint constraintWithItem:v1 attribute:ax relatedBy:re toItem:v2 attribute:ax multiplier:1.0f constant:0.0f]]; // Center X
+			}
+			
+			[self addConstraints:constraints];
+		}
+		
+		// Saves the new constraints.
+		[self.constraints setArray:constraints];
+	}
+	
+	[super updateConstraints];
+}
 
 @end
 NS_ASSUME_NONNULL_END
