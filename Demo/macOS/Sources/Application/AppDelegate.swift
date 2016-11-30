917399d3629863ca043ae9b4b7b0b7a7a6ff3304
Jacopo Filié <Jackfelle@users.noreply.github.com>
2016-11-30 14:00:45 +0100

    MOD: updated the projects files to be only Xcode 6.3 compatible. MOD: resetted the Demo project and added targets for iOS, macOS and tvOS.

---
+++ Demo/macOS/Sources/Application/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOS
+//
+//  Created by Jacopo Filié on 29/11/2016.
+//  Copyright © 2016 Jacopo Filié. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
