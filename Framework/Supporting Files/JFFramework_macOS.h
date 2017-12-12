d0b56ab8226d70c2c182b2d4f1b143bce27b9346
Jacopo Filié <Jackfelle@users.noreply.github.com>
2017-12-12 23:01:45 +0100

    ADD: imported the preprocessor macros for APIs availability and target conditioned compilation.

--- Framework/Supporting Files/JFFramework_macOS.h
+++ Framework/Supporting Files/JFFramework_macOS.h
@@ -1,5 +1,32 @@
+//
+//	The MIT License (MIT)
+//
+//	Copyright © 2015-2017 Jacopo Filié
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
+//
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #import <Cocoa/Cocoa.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //! Project version number for JFFramework_macOS.
 FOUNDATION_EXPORT double JFFramework_macOSVersionNumber;
@@ -7,6 +34,8 @@ FOUNDATION_EXPORT double JFFramework_macOSVersionNumber;
 //! Project version string for JFFramework_macOS.
 FOUNDATION_EXPORT const unsigned char JFFramework_macOSVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <JFFramework_macOS/PublicHeader.h>
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#import <JFFramework_macOS/JFPreprocessorMacros.h>
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
