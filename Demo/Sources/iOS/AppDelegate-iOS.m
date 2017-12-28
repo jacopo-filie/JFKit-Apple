56f260377cbfffeaf202307c0d9a533ff4be8ba9
Jacopo Filié <Jackfelle@users.noreply.github.com>
2017-12-28 18:04:58 +0100

    MOD: refactored the iOS Demo-ObjC project.

--- Demo/Sources/iOS/AppDelegate-iOS.m
+++ Demo/Sources/iOS/AppDelegate-iOS.m
@@ -1,112 +1,158 @@
+//
+//	The MIT License (MIT)
+//
+//	Copyright © 2017 Jacopo Filié
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
+//
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #import "AppDelegate-iOS.h"
+
 #import "DetailViewController.h"
 #import "MasterViewController.h"
 
-@interface AppDelegate () <UISplitViewControllerDelegate>
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+NS_ASSUME_NONNULL_BEGIN
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@interface AppDelegate ()
 
 @end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma mark -
 
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	// Override point for customization after application launch.
-	UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
-	UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
-	navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
-	splitViewController.delegate = self;
-
-	UINavigationController *masterNavigationController = splitViewController.viewControllers[0];
-	MasterViewController *controller = (MasterViewController *)masterNavigationController.topViewController;
-	controller.managedObjectContext = self.persistentContainer.viewContext;
-	return YES;
-}
-
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-	// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-	// Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-	// Saves changes in the application's managed object context before the application terminates.
-	[self saveContext];
-}
-
-
-#pragma mark - Split view
-
-- (BOOL)splitViewController:(UISplitViewController *)splitViewController collapseSecondaryViewController:(UIViewController *)secondaryViewController ontoPrimaryViewController:(UIViewController *)primaryViewController {
-    if ([secondaryViewController isKindOfClass:[UINavigationController class]] && [[(UINavigationController *)secondaryViewController topViewController] isKindOfClass:[DetailViewController class]] && ([(DetailViewController *)[(UINavigationController *)secondaryViewController topViewController] detailItem] == nil)) {
-        // Return YES to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-        return YES;
-    } else {
-        return NO;
-    }
-}
-
-#pragma mark - Core Data stack
+// =================================================================================================
+// MARK: Properties - Stores
+// =================================================================================================
 
 @synthesize persistentContainer = _persistentContainer;
 
-- (NSPersistentContainer *)persistentContainer {
-    // The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it.
-    @synchronized (self) {
-        if (_persistentContainer == nil) {
-            _persistentContainer = [[NSPersistentContainer alloc] initWithName:@"Database-ObjC"];
-            [_persistentContainer loadPersistentStoresWithCompletionHandler:^(NSPersistentStoreDescription *storeDescription, NSError *error) {
-                if (error != nil) {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    
-                    /*
-                     Typical reasons for an error here include:
-                     * The parent directory does not exist, cannot be created, or disallows writing.
-                     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                     * The device is out of space.
-                     * The store could not be migrated to the current model version.
-                     Check the error message to determine what the actual problem was.
-                    */
-                    NSLog(@"Unresolved error %@, %@", error, error.userInfo);
-                    abort();
-                }
-            }];
-        }
-    }
-    
-    return _persistentContainer;
+// =================================================================================================
+// MARK: Properties (Inherited) - User interface
+// =================================================================================================
+
+@synthesize window = _window;
+
+// =================================================================================================
+// MARK: Properties accessors - Stores
+// =================================================================================================
+
+- (NSPersistentContainer*)persistentContainer
+{
+	// The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it.
+	@synchronized(self)
+	{
+		if(!_persistentContainer)
+		{
+			_persistentContainer = [[NSPersistentContainer alloc] initWithName:@"Database-ObjC"];
+			[_persistentContainer loadPersistentStoresWithCompletionHandler:^(NSPersistentStoreDescription* storeDescription, NSError* error) {
+				if(!error)
+					return;
+				
+				// Replace this implementation with code to handle the error appropriately.
+				// abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+				
+				/*
+				 Typical reasons for an error here include:
+				 * The parent directory does not exist, cannot be created, or disallows writing.
+				 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+				 * The device is out of space.
+				 * The store could not be migrated to the current model version.
+				 Check the error message to determine what the actual problem was.
+				 */
+				NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+				abort();
+			}];
+		}
+	}
+	
+	return _persistentContainer;
 }
 
-#pragma mark - Core Data Saving support
+// =================================================================================================
+// MARK: Methods - Stores management
+// =================================================================================================
 
-- (void)saveContext {
-    NSManagedObjectContext *context = self.persistentContainer.viewContext;
-    NSError *error = nil;
-    if ([context hasChanges] && ![context save:&error]) {
-        // Replace this implementation with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
-        abort();
-    }
+- (void)saveContext
+{
+	NSManagedObjectContext* context = self.persistentContainer.viewContext;
+	
+	NSError* error = nil;
+	if([context hasChanges] && ![context save:&error])
+	{
+		// Replace this implementation with code to handle the error appropriately.
+		// abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+		NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+		abort();
+	}
+}
+
+// =================================================================================================
+// MARK: Protocols (UIApplicationDelegate)
+// =================================================================================================
+
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary<UIApplicationLaunchOptionsKey, id>* __nullable)launchOptions
+{
+	UISplitViewController* splitViewController = (UISplitViewController*)self.window.rootViewController;
+	UINavigationController* navigationController = [splitViewController.viewControllers lastObject];
+	navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
+	splitViewController.delegate = self;
+	
+	UINavigationController* masterNavigationController = splitViewController.viewControllers[0];
+	MasterViewController* controller = (MasterViewController*)masterNavigationController.topViewController;
+	controller.managedObjectContext = self.persistentContainer.viewContext;
+	
+	return YES;
+}
+
+- (void)applicationWillTerminate:(UIApplication*)application
+{
+	[self saveContext];
+}
+
+// =================================================================================================
+// MARK: Protocols (UISplitViewControllerDelegate)
+// =================================================================================================
+
+- (BOOL)splitViewController:(UISplitViewController*)splitViewController collapseSecondaryViewController:(UIViewController*)secondaryViewController ontoPrimaryViewController:(UIViewController*)primaryViewController
+{
+	if(![secondaryViewController isKindOfClass:[UINavigationController class]])
+		return NO;
+	
+	if(![[(UINavigationController*)secondaryViewController topViewController] isKindOfClass:[DetailViewController class]])
+		return NO;
+	
+	return ![(DetailViewController*)[(UINavigationController*)secondaryViewController topViewController] detailItem];
 }
 
 @end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+NS_ASSUME_NONNULL_END
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
