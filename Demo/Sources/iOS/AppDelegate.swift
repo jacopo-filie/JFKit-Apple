f3cc00995675b8921065b96c5d46ba8f925fa15c
Jacopo Filié <Jackfelle@users.noreply.github.com>
2018-01-03 01:38:17 +0100

    MOD: updated the license date.

--- Demo/Sources/iOS/AppDelegate.swift
+++ Demo/Sources/iOS/AppDelegate.swift
@@ -1,5 +1,5 @@
 //
-//	Copyright © 2017 Jacopo Filié
+//	Copyright © 2017-2018 Jacopo Filié
 //
 //	Permission is hereby granted, free of charge, to any person obtaining a copy
 //	of this software and associated documentation files (the "Software"), to deal
