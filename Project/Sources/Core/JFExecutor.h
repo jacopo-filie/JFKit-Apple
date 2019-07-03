2ad3d0d24848d1e3ff6299a22cf9de02eaa0e139
Jacopo Filié <Jackfelle@users.noreply.github.com>
2019-07-03 02:35:13 +0200

    ADD: added "JFExecutor" class.

---
+++ Project/Sources/Core/JFExecutor.h
@@ -0,0 +1,174 @@
+//
+//	The MIT License (MIT)
+//
+//	Copyright © 2019 Jacopo Filié
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
+//
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@import Foundation;
+
+#import "JFCompletions.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+NS_ASSUME_NONNULL_BEGIN
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// =================================================================================================
+// MARK: Macros
+// =================================================================================================
+
+/**
+ * A convenient macro that represents a block used as a `JFExecutor` method parameter type.
+ */
+#define JFExecutorBlockParameter void (^)(OwnerType owner)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma mark -
+
+/**
+ * An executor can be used to garantee serialized execution of operation blocks. It is tipically used inside other objects to perform internal operations without using expensive synchronization utilities (e.g.: locks).
+ * `OwnerType` defines the type of the parameter `owner` of the executor block.
+ */
+@interface JFExecutor<__covariant OwnerType> : NSObject
+
+// =================================================================================================
+// MARK: Properties - Observers
+// =================================================================================================
+
+#if JF_WEAK_ENABLED
+/**
+ * The owner of the executor.
+ */
+@property (weak, nonatomic, readonly, nullable) OwnerType owner;
+#else
+/**
+ * The owner of the executor.
+ * @warning Remember to use `-clearOwner` to unset the owner when it is not available anymore because it may become a dangling pointer otherwise.
+ */
+@property (unsafe_unretained, nonatomic, readonly, nullable) OwnerType owner;
+#endif
+
+// =================================================================================================
+// MARK: Methods - Memory
+// =================================================================================================
+
+/**
+ * NOT AVAILABLE
+ */
++ (instancetype)new NS_UNAVAILABLE;
+
+/**
+ * NOT AVAILABLE
+ */
+- (instancetype)init NS_UNAVAILABLE;
+
+/**
+ * Initializes the executor with the given owner.
+ * @param owner The owner of the executor.
+ * @return The initialized executor.
+ */
+- (instancetype)initWithOwner:(OwnerType)owner NS_DESIGNATED_INITIALIZER;
+
+// =================================================================================================
+// MARK: Methods - Observers
+// =================================================================================================
+
+/**
+ * Cancels all enqueued blocks and unsets the owner of the executor.
+ * @warning On systems where the `weak` keyword is not available, you can use this method to prevent the risk of a dangling pointer creation.
+ */
+- (void)clearOwner;
+
+// =================================================================================================
+// MARK: Methods - Service
+// =================================================================================================
+
+/**
+ * Enqueues the given executor block; `block` will not be executed in case of internal error.
+ * @param block The block to enqueue.
+ */
+- (void)enqueue:(JFExecutorBlockParameter)block;
+
+/**
+ * Enqueues the given executor block; `failureBlock` will be executed instead of `block` in case of internal error.
+ * @param block The block to enqueue.
+ * @param failureBlock The block to execute in case of internal error.
+ */
+- (void)enqueue:(JFExecutorBlockParameter)block failureBlock:(JFFailureBlock __nullable)failureBlock;
+
+/**
+ * Enqueues the given executor block; `failureBlock` will be executed instead of `block` in case of internal error.
+ * @param block The block to enqueue.
+ * @param failureBlock The block to execute in case of internal error.
+ * @param waitUntilFinished `YES` if the calling thread should wait until `block` or `failureBlock` are executed, `NO` otherwise.
+ */
+- (void)enqueue:(JFExecutorBlockParameter)block failureBlock:(JFFailureBlock __nullable)failureBlock waitUntilFinished:(BOOL)waitUntilFinished;
+
+/**
+ * Enqueues the given executor block; `block` will not be executed in case of internal error.
+ * @param block The block to enqueue.
+ * @param waitUntilFinished `YES` if the calling thread should wait until `block` is executed, `NO` otherwise.
+ */
+- (void)enqueue:(JFExecutorBlockParameter)block waitUntilFinished:(BOOL)waitUntilFinished;
+
+/**
+ * Executes the given executor block as soon as possible; `block` will not be executed in case of internal error.
+ * @param block The block to execute or enqueue.
+ * @warning If the current thread queue is not the internal executor queue, `block` will be enqueued on the internal executor queue.
+ */
+- (void)execute:(JFExecutorBlockParameter)block;
+
+/**
+ * Executes the given executor block as soon as possible; `failureBlock` will be executed instead of `block` in case of internal error.
+ * @param block The block to execute or enqueue.
+ * @param failureBlock The block to execute in case of internal error.
+ * @warning If the current thread queue is not the internal executor queue, `block` will be enqueued on the internal executor queue.
+ */
+- (void)execute:(JFExecutorBlockParameter)block failureBlock:(JFFailureBlock __nullable)failureBlock;
+
+/**
+ * Executes the given executor block as soon as possible; `failureBlock` will be executed instead of `block` in case of internal error.
+ * @param block The block to execute or enqueue.
+ * @param failureBlock The block to execute in case of internal error.
+ * @param waitUntilFinished `YES` if the calling thread should wait until `block` or `failureBlock` are executed, `NO` otherwise.
+ * @warning If the current thread queue is not the internal executor queue, `block` will be enqueued on the internal executor queue.
+ */
+- (void)execute:(JFExecutorBlockParameter)block failureBlock:(JFFailureBlock __nullable)failureBlock waitUntilFinished:(BOOL)waitUntilFinished;
+
+/**
+ * Executes the given executor block as soon as possible; `block` will not be executed in case of internal error.
+ * @param block The block to execute or enqueue.
+ * @param waitUntilFinished `YES` if the calling thread should wait until `block` is executed, `NO` otherwise.
+ * @warning If the current thread queue is not the internal executor queue, `block` will be enqueued on the internal executor queue.
+ */
+- (void)execute:(JFExecutorBlockParameter)block waitUntilFinished:(BOOL)waitUntilFinished;
+
+@end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+NS_ASSUME_NONNULL_END
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ Project/Sources/Core/JFExecutor.m
@@ -0,0 +1,216 @@
+//
+//	The MIT License (MIT)
+//
+//	Copyright © 2019 Jacopo Filié
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
+//
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#import "JFExecutor.h"
+
+#import "JFShortcuts.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+NS_ASSUME_NONNULL_BEGIN
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@interface JFExecutor<__covariant OwnerType> (/* Private */)
+
+// =================================================================================================
+// MARK: Properties - Observers
+// =================================================================================================
+
+#if JF_WEAK_ENABLED
+@property (weak, nonatomic, readwrite, nullable) OwnerType owner;
+#else
+@property (unsafe_unretained, nonatomic, readwrite, nullable) OwnerType owner;
+#endif
+
+// =================================================================================================
+// MARK: Properties - Service
+// =================================================================================================
+
+@property (strong, nonatomic, readonly) NSOperationQueue* queue;
+
+// =================================================================================================
+// MARK: Methods - Utilities
+// =================================================================================================
+
++ (NSError*)newCanceledError;
++ (NSError*)newDeallocatedError;
+
+@end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma mark -
+
+@implementation JFExecutor
+
+// =================================================================================================
+// MARK: Properties - Observers
+// =================================================================================================
+
+@synthesize owner = _owner;
+
+// =================================================================================================
+// MARK: Properties - Service
+// =================================================================================================
+
+@synthesize queue = _queue;
+
+// =================================================================================================
+// MARK: Methods - Memory
+// =================================================================================================
+
+- (void)dealloc
+{
+	[self.queue cancelAllOperations];
+}
+
+- (instancetype)initWithOwner:(id)owner
+{
+	self = [super init];
+	
+	NSOperationQueue* queue = [NSOperationQueue new];
+	queue.maxConcurrentOperationCount = 1;
+	queue.name = ClassName;
+	
+	_owner = owner;
+	_queue = queue;
+	
+	return self;
+}
+
+// =================================================================================================
+// MARK: Methods - Observers
+// =================================================================================================
+
+- (void)clearOwner
+{
+	[self.queue cancelAllOperations];
+	self.owner = nil;
+}
+
+// =================================================================================================
+// MARK: Methods - Service
+// =================================================================================================
+
+- (void)enqueue:(void (^)(id owner))block
+{
+	[self enqueue:block failureBlock:nil waitUntilFinished:NO];
+}
+
+- (void)enqueue:(void (^)(id owner))block failureBlock:(JFFailureBlock __nullable)failureBlock
+{
+	[self enqueue:block failureBlock:failureBlock waitUntilFinished:NO];
+}
+
+- (void)enqueue:(void (^)(id owner))block failureBlock:(JFFailureBlock __nullable)failureBlock waitUntilFinished:(BOOL)waitUntilFinished
+{
+	BOOL __block executed = NO;
+	
+#if JF_WEAK_ENABLED
+	JFWeakify(self.owner, Owner);
+#else
+	__typeof(self) __strong strongOwner = self.owner;
+#endif
+	NSBlockOperation* operation = [NSBlockOperation blockOperationWithBlock:^{
+		executed = YES;
+		
+#if JF_WEAK_ENABLED
+		JFStrongify(weakOwner, Owner);
+#endif
+		if(strongOwner)
+		{
+			block(strongOwner);
+			return;
+		}
+		
+		if(failureBlock)
+			failureBlock([JFExecutor newDeallocatedError]);
+	}];
+	
+	if(failureBlock)
+	{
+		operation.completionBlock = ^{
+			if(!executed)
+				failureBlock([JFExecutor newCanceledError]);
+		};
+	}
+	
+	[self.queue addOperations:@[operation] waitUntilFinished:waitUntilFinished];
+}
+
+- (void)enqueue:(void (^)(id owner))block waitUntilFinished:(BOOL)waitUntilFinished
+{
+	[self enqueue:block failureBlock:nil waitUntilFinished:waitUntilFinished];
+}
+
+- (void)execute:(void (^)(id owner))block
+{
+	[self execute:block failureBlock:nil waitUntilFinished:NO];
+}
+
+- (void)execute:(void (^)(id owner))block failureBlock:(JFFailureBlock __nullable)failureBlock
+{
+	[self execute:block failureBlock:failureBlock waitUntilFinished:NO];
+}
+
+- (void)execute:(void (^)(id owner))block failureBlock:(JFFailureBlock __nullable)failureBlock waitUntilFinished:(BOOL)waitUntilFinished
+{
+	if([NSOperationQueue currentQueue] != self.queue)
+	{
+		[self enqueue:block failureBlock:failureBlock waitUntilFinished:waitUntilFinished];
+		return;
+	}
+	
+	id owner = self.owner;
+	if(owner)
+		block(owner);
+	else if(failureBlock)
+		failureBlock([JFExecutor newDeallocatedError]);
+}
+
+- (void)execute:(void (^)(id owner))block waitUntilFinished:(BOOL)waitUntilFinished
+{
+	[self execute:block failureBlock:nil waitUntilFinished:waitUntilFinished];
+}
+
++ (NSError*)newCanceledError
+{
+	return [NSError errorWithDomain:ClassName code:NSIntegerMax userInfo:nil]; // TODO: create "canceled" error.
+}
+
++ (NSError*)newDeallocatedError
+{
+	return [NSError errorWithDomain:ClassName code:NSIntegerMax userInfo:nil]; // TODO: create "deallocated" error.
+}
+
+@end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+NS_ASSUME_NONNULL_END
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
